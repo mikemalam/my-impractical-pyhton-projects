06b3f32f969ae5aee62ad077efe944cd0e6391f0
mikemalam <mikemalam@tutanota.com>
2019-04-20 12:36:00 +0200

    Just copied code.

--- finding-palingram-spells/load_dictionnary.py
+++ finding-palingram-spells/load_dictionnary.py
@@ -12,3 +12,15 @@ Returns:
 Requires-import sys
 
 """
+import sys
+
+def load(file):
+    """pen a text file & return a list of lowercase strings."""
+    try:
+        with open(file) as in_file:
+            loaded_txt = in_file.read().strip().split('\n')
+            loaded_txt = [x.lower() for x in loaded_txt]
+            return laded_txt
+        except IOError as e:
+            print("{}\nError opening {}. Terminating program.".format(e, file),file=sys.stderr)
+            sys.exit(1)
