290eb0858b72c0194dbc50fe2ee98944553338bb
mikemalam <mikemalam@tutanota.com>
2019-04-20 12:19:43 +0200

    Created file

---
+++ finding-palingram-spells/load_dictionnary.py
@@ -0,0 +1,14 @@
+"""Load a text file as a list.
+
+Arguments:
+-text file name (adn directory path, if needed)
+
+Exceptions:
+-IOError if filename not found.
+
+Returns:
+-A list of all words in a text file in lower case.
+
+Requires-import sys
+
+"""
