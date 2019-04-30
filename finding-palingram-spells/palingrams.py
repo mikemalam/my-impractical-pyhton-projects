cc57c6ff7bf4faba082df76ddc59257801899fc0
mikemalam <mikemalam@tutanota.com>
2019-04-30 21:58:18 +0200

    removed first parameter '1' in range function to include more palingrams.

--- finding-palingram-spells/palingrams.py
+++ finding-palingram-spells/palingrams.py
@@ -10,15 +10,18 @@ def main():
     for word in word_list:
         end = len(word)
         rev_word = word[::-1]
-        if word_length > 1:
-            for i in range(1, word_length):
+        if end > 1:
+            for i in range(end):
                 if word[i:] == rev_word[:end-i] and rev_word[end-i] in word_list:
                     palingrams_list.append((word, rev_word[end-i:]))
-                if word[:i] == rev_word[end-i:] and rev_worp[:end-i] in word_lis: 
-                    palingrams_list.append((rev_worp[:end - i], word))
-    sorted(palingrams_list, key=str.lower)
+                if word[:i] == rev_word[end-i:] and rev_word[:end-i] in word_list: 
+                    palingrams_list.append((rev_word[:end - i], word))
+    sorted(palingrams_list)
+
+    #Display list of palingrams.
     print('\nNumber of palingrams = {}\n'.format(len(palingrams_list)))
-    print(*palingrams_list, sep="\n")
-    
+    for first, second in palingrams_list:
+        print("{} {}".format(first, second))
+
 if __name__ == "__main__":
     main()
