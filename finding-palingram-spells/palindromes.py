5384016bc689a511981f72ddd06fb43aefab6196
mikemalam <mikemalam@tutanota.com>
2019-04-30 20:10:22 +0200

    create a main function to improve pylint score

--- finding-palingram-spells/palindromes.py
+++ finding-palingram-spells/palindromes.py
@@ -1,11 +1,17 @@
 """Find palindromes (letter palingrams) in a dictionary file."""
 import load_dictionnary
-word_list = load_dictionnary.load('liste.de.mots.francais.frgut.txt')
-pali_list = []
 
-for word in word_list:
-    if len(word) > 1 and word == word[::-1]:
-        pali_list.append(word)
+def main():
+    """main programm"""
+    word_list = load_dictionnary.load('liste.de.mots.francais.frgut.txt')
+    pali_list = []
 
-print('The dictionnary contains {} palindromes'.format(len(pali_list)))
-print(*pali_list, sep='\n')
+    for word in word_list:
+        if len(word) > 1 and word == word[::-1]:
+            pali_list.append(word)
+
+    print('The dictionnary contains {} palindromes'.format(len(pali_list)))
+    print(*pali_list, sep='\n')
+
+if __name__ == "__main__":
+    main()
