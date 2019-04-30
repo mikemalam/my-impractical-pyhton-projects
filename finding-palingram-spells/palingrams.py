ade1d8fe338793ea4ce5746c6c0a480fa95ef300
mikemalam <mikemalam@tutanota.com>
2019-04-30 21:35:47 +0200

    correction of do boucles with book correction

--- finding-palingram-spells/palingrams.py
+++ finding-palingram-spells/palingrams.py
@@ -8,14 +8,16 @@ def main():
     palingrams_list = []
 
     for word in word_list:
-        word_length = len(word)
+        end = len(word)
+        rev_word = word[::-1]
         if word_length > 1:
             for i in range(1, word_length):
-                if word[i::-1] in word_list and word[:i-1:-1] == word[i::]:
-                    palingrams_list.append(word[i::-1] + ' ' + word)
-                if word[i::] in word_list and word[i::-1] == word[:i+1:]:
-                    palingrams_list.append(word + ' ' + word[i::-1])
+                if word[i:] == rev_word[:end-i] and rev_word[end-i] in word_list:
+                    palingrams_list.append((word, rev_word[end-i:]))
+                if word[:i] == rev_word[end-i:] and rev_worp[:end-i] in word_lis: 
+                    palingrams_list.append((rev_worp[:end - i], word))
     sorted(palingrams_list, key=str.lower)
+    print('\nNumber of palingrams = {}\n'.format(len(palingrams_list)))
     print(*palingrams_list, sep="\n")
     
 if __name__ == "__main__":
