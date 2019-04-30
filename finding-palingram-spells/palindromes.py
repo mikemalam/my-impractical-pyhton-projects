0ab7074c97ffa533803d40b1ff5a4f9c95a24818
mikemalam <mikemalam@tutanota.com>
2019-04-30 15:51:43 +0200

    corrected wrong variable name

--- finding-palingram-spells/palindromes.py
+++ finding-palingram-spells/palindromes.py
@@ -4,7 +4,7 @@ word_list = load_dictionnary.load('2of4brif.txt')
 pali_list = []
 
 for word in word_list:
-    if len(xords) > 1 and word == word[::-1]:
+    if len(word) > 1 and word == word[::-1]:
         pali_list.append(word)
 
 print('The dictionnary contains {} palindromes'.format(len(pali_list)))
