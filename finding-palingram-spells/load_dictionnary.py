e69f50bf13530d4a6026413bf0a34ac8e1b48e4c
mikemalam <mikemalam@tutanota.com>
2019-04-30 15:44:27 +0200

    tabulation error

--- finding-palingram-spells/load_dictionnary.py
+++ finding-palingram-spells/load_dictionnary.py
@@ -21,6 +21,6 @@ def load(file):
             loaded_txt = in_file.read().strip().split('\n')
             loaded_txt = [x.lower() for x in loaded_txt]
             return loaded_txt
-        except IOError as e:
-            print("{}\nError opening {}. Terminating program.".format(e, file),file=sys.stderr)
-            sys.exit(1)
+    except IOError as e:
+        print("{}\nError opening {}. Terminating program.".format(e, file),file=sys.stderr)
+        sys.exit(1)
