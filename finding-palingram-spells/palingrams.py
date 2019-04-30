871ab179f5cd6b33da3d9f845330600b59fffadf
mikemalam <mikemalam@tutanota.com>
2019-04-30 22:38:54 +0200

    change lists to sets for optimization.

--- finding-palingram-spells/palingrams.py
+++ finding-palingram-spells/palingrams.py
@@ -1,20 +1,22 @@
 """Find palingrams in an text dictionary."""
 
 import load_dictionnary
+import time
 
 def main():
     """Main function"""
-    word_list = load_dictionnary.load("2of4brif.txt")
+    word_list = load_dictionnary.load("liste.de.mots.francais.frgut.txt")
+    words = set(word_list)
     palingrams_list = []
 
-    for word in word_list:
+    for word in words:
         end = len(word)
         rev_word = word[::-1]
         if end > 1:
             for i in range(end):
-                if word[i:] == rev_word[:end-i] and rev_word[end-i] in word_list:
+                if word[i:] == rev_word[:end-i] and rev_word[end-i:] in words:
                     palingrams_list.append((word, rev_word[end-i:]))
-                if word[:i] == rev_word[end-i:] and rev_word[:end-i] in word_list: 
+                if word[:i] == rev_word[end-i:] and rev_word[:end-i] in words: 
                     palingrams_list.append((rev_word[:end - i], word))
     sorted(palingrams_list)
 
@@ -24,4 +26,8 @@ def main():
         print("{} {}".format(first, second))
 
 if __name__ == "__main__":
+    start_time = time.time()
     main()
+    end_time = time.time()
+    print('Runtime for this program was {} seconds.'.format(end_time-start_time))
+
