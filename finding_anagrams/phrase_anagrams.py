5be9982626156ffaa174bb0757c137daab43fdfa
mikemalam <mikemalam@tutanota.com>
2019-05-08 12:23:36 +0200

    initial commit

---
+++ finding_anagrams/phrase_anagrams.py
@@ -0,0 +1,17 @@
+"""Find anagrams from an input od user in a french dictionnary."""
+
+import sys
+from Collection import Counter
+
+def find_anagrams():
+    """Function to get annagram in word list"""
+
+def process_choice():
+    """Function to process choice of user."""
+
+def main():
+    """Main function. Interact with user to choose the best combinations."""
+
+if __name__ == "__main__":
+    main()
+
